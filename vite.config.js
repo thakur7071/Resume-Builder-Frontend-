bee251e881299f5a74aadf17c31a96171c1ec086
thakur7071 <singhnaveen2370@gmail.com>
2024-09-20 12:58:11 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +8,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    
   },
-})
+  build: {
+    rollupOptions: {
+      external: ['axios'], // Add axios to the external array
+    },
+  },
+});
