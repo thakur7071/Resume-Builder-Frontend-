e3b163e9b9285306a119da124efdfa542f5817c3
thakur7071 <singhnaveen2370@gmail.com>
2025-01-03 23:17:45 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
+
+process.env.VITE_CLERK_PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY || 'pk_test_d29ya2FibGUtbWFuYXRlZS05OS5jbGVyay5hY2NvdW50cy5kZXYk';
+
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
